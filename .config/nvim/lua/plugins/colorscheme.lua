12a980c044dc7d8d06699042160f5c678b081974
Tadashi Matsuura <4456384+tadashi1105@users.noreply.github.com>
2024-08-07 17:25:02 +0900

    feat: add tokyonight color scheme to alacritty and tmux
    - Added `tokyonight` color scheme configuration to Alacritty.
    - Updated tmux configuration to include `tokyonight` color scheme.

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+-- stylua: ignore
+-- if true then return {} end
+
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
