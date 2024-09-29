0a5e991b08412d6c09b671051324ce3d290dfdde
Tadashi Matsuura <4456384+tadashi1105@users.noreply.github.com>
2024-09-29 14:46:22 +0900

    feat(nvim): display filename in the top-right corner for better visibility
    Added the incline.nvim plugin to show the filename in the top-right corner.
    This makes it easier to confirm the current file, especially in larger setups
    or when multiple buffers are open. Icons are added using mini.icons, and
    the modified state of the file is also indicated.

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,5 +1,34 @@
 return {
   {
+    "b0o/incline.nvim",
+    dependencies = { "echasnovski/mini.icons" },
+    event = "VeryLazy",
+    opts = {
+      window = {
+        padding = 1,
+        margin = { vertical = 0, horizontal = 0 },
+      },
+      hide = {
+        cursorline = true,
+      },
+      render = function(props)
+        local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+        local modified = vim.bo[props.buf].modified
+        local icon, icon_hl = MiniIcons.get("file", filename)
+
+        return {
+          icon and { " ", icon, group = icon_hl } or "",
+          " ",
+          filename,
+          modified and { " [+]", group = "BufferLineModified" } or "",
+          " ",
+          group = "CursorLine",
+        }
+      end,
+    },
+  },
+
+  {
     "folke/zen-mode.nvim",
     cmd = "ZenMode",
     opts = {},
