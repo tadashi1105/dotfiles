974e43b23ebcf765c83888f4da539b0c8a06e69b
Tadashi Matsuura <4456384+tadashi1105@users.noreply.github.com>
2026-09-20 11:51:15 +0900

    refactor(nvim): disable image markdown integration and obsidian plugin

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -18,7 +18,7 @@ return {
       backend = "kitty",
       integrations = {
         markdown = {
-          enabled = true,
+          enabled = false,
           clear_in_insert_mode = false,
           download_remote_images = true,
           only_render_image_at_cursor = true,
